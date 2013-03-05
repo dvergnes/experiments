48daf07d967835f769feab0154f6b9fffe4fce62
vergnes <denis.vergnes@gmail.com>
2013-03-05 17:13:26 +0800

    Fix NPE

--- training/src/main/java/experiments/SumResolver.java
+++ training/src/main/java/experiments/SumResolver.java
@@ -1,6 +1,8 @@
 package experiments;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -74,18 +76,38 @@ public class SumResolver {
 	}
 
 	public static Set<Pair> findPairs(List<Integer> numbers, int total) {
-		Collection<Pair> allPairs = new LinkedList<SumResolver.Pair>();
-		for (int i = 0; i < numbers.size(); i++) {
-			for (int j = 0; j < numbers.size(); j++) {
-				if (i != j) {
-					allPairs.add(new Pair(numbers.get(i), numbers.get(j)));
+		Set<Pair> result = new HashSet<SumResolver.Pair>();
+		if (numbers != null) {
+			Collection<Pair> allPairs = new LinkedList<SumResolver.Pair>();
+			for (int i = 0; i < numbers.size(); i++) {
+				for (int j = 0; j < numbers.size(); j++) {
+					if (i != j) {
+						allPairs.add(new Pair(numbers.get(i), numbers.get(j)));
+					}
+				}
+			}
+			for (Pair pair : allPairs) {
+				if (pair.sum() == total) {
+					result.add(pair);
 				}
 			}
 		}
+		return result;
+	}
+
+	public static Set<Pair> findPairsV2(List<Integer> numbers, int total) {
 		Set<Pair> result = new HashSet<SumResolver.Pair>();
-		for (Pair pair : allPairs) {
-			if (pair.sum() == total) {
-				result.add(pair);
+		if (numbers != null) {
+			List<Integer> sortedNumbers = new ArrayList<Integer>(numbers);
+			Collections.sort(sortedNumbers);
+			for (int i = 0; i < sortedNumbers.size(); i++) {
+				int currentElement = sortedNumbers.get(i);
+				int potentialPairElement = total - currentElement;
+				int j = Collections.binarySearch(sortedNumbers,
+						potentialPairElement);
+				if (j > 0 && j != i) {
+					result.add(new Pair(currentElement, potentialPairElement));
+				}
 			}
 		}
 		return result;
--- training/src/test/java/experiments/SumResolverTest.java
+++ training/src/test/java/experiments/SumResolverTest.java
@@ -12,11 +12,30 @@ import experiments.SumResolver.Pair;
 
 public class SumResolverTest {
 
+	private final List<Integer> numbers = Arrays.asList(1, 6, 5, 8, 3, 13, 9,
+			-1, 10, 4);
+	private final int total = 12;
+
+	@Test
+	public void testFindPairsEmpty() {
+		assertThat(SumResolver.findPairs(null, total)).isEmpty();
+	}
+
+	@Test
+	public void testFindPairsV2Empty() {
+		assertThat(SumResolver.findPairsV2(null, total)).isEmpty();
+	}
+
 	@Test
 	public void testFindPairs() {
-		List<Integer> numbers = Arrays.asList(1, 6, 5, 8, 3, 13, 9, -1, 10, 4);
-		int total = 12;
 		Set<Pair> pairs = SumResolver.findPairs(numbers, total);
+		assertThat(pairs).hasSize(3).contains(new SumResolver.Pair(3, 9),
+				new SumResolver.Pair(-1, 13), new SumResolver.Pair(4, 8));
+	}
+
+	@Test
+	public void testFindPairsV2() {
+		Set<Pair> pairs = SumResolver.findPairsV2(numbers, total);
 		assertThat(pairs).hasSize(3).contains(new SumResolver.Pair(3, 9),
 				new SumResolver.Pair(-1, 13), new SumResolver.Pair(4, 8));
 	}
